945d19709cd5ac5468f72078d6bb0bc6daa992ae
adina9 <adinazwe@gmail.com>
2022-01-02 22:40:04 +0200

    finishing

---
+++ src/cmps/ToggleWrapper.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import NightsStayIcon from '@material-ui/icons/NightsStay';
+
+export const ToggleWrapper = ({ value, onUpdate }) => {
+    return (
+        <div className="toggle-wrapper flex a-center j-center">
+            <div className="toggle checkcross pa">
+                <input type="text" />
+                <label className="toggle-item" style={{ backgroundColor: value ? '#969696b5' : '#ffeaab' }} onClick={() => onUpdate(!value)}>
+                    <div className={value ? 'check' : 'uncheck'}>{value ? <NightsStayIcon /> : <WbSunnyIcon />}</div>
+                </label>
+            </div>
+        </div>
+    );
+}
